0d152082ba9e7d1478b2c4957dfccab7db2dd90e
Bruno Rocha <brunorocha2674@gmail.com>
2022-12-12 15:48:55 -0300

    chore: update

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+/**
+ * Alias for production environment
+ * Uncomment before deploy
+ */
+// import 'module-alias/register'
 import { createServer, IncomingMessage, ServerResponse } from 'http'
 
 const port = process.env.APP_PORT || '3000'
